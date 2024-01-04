972cfa4f4b7c0d785d0533654593d85a61583aeb
INB-OJpineda <154305508+OJPineda26@users.noreply.github.com>
2024-01-04 04:31:43 -1200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -89,10 +89,9 @@ int Multiplication(int num1, int num2) {
 }
 
 int Division(int num1, int num2) {
-    if (num2 != 0) {
+    
         return num1 / num2;
-    } else {
-        cout << "Error: Division by zero.\n";
+
         return 0;
-    }
 }
+
